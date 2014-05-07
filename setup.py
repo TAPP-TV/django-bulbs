75fd1e7c860c9d997d45f29c5bfc94dd3430cb37
Chris Sinchok <csinchok@theonion.com>
2014-05-07 16:02:21 -0500

    You know what it is

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requires = [
     "Django>=1.4",
     "Pillow==1.7.8",
     "South==0.8.1",
-    "betty-cropper==0.1.19",
+    "betty-cropper==0.1.20",
     "django-elastimorphic==0.0.2",
     "django-json-field==0.5.5",
     "django_polymorphic==0.5.1",
