2100a48dfd0a96718027be64c9f4183d7bdd811d
Chris Sinchok <csinchok@theonion.com>
2014-05-08 11:27:19 -0500

    Converging upon master

--- bulbs/videos/models.py
+++ bulbs/videos/models.py
@@ -34,7 +34,6 @@ class Video(models.Model):
     name = models.CharField(max_length=255)
     data = JSONField(null=True, blank=True, help_text="This is JSON that is returned from an encoding job")
     sources = JSONField(null=True, blank=True, default=[], help_text="This is a JSON array of sources.")
-    poster = models.CharField(null=True, blank=True, max_length=255)
     poster_url = models.URLField(null=True, blank=True)
     status = models.IntegerField(choices=STATUSES, default=NOT_STARTED)
     job_id = models.IntegerField(null=True, blank=True, help_text="The zencoder job ID")
