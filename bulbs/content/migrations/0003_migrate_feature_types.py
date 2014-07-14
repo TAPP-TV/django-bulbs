4713e0412044084f35ed0d50dfa072ecb12484f8
Chris Sinchok <csinchok@theonion.com>
2014-07-14 15:23:36 -0500

    Migrations are not fun

--- bulbs/content/migrations/0003_migrate_feature_types.py
+++ bulbs/content/migrations/0003_migrate_feature_types.py
@@ -5,6 +5,21 @@ from south.v2 import DataMigration
 from django.db import models
 from django.utils.text import slugify
 
+# Safe User import for Django < 1.5
+try:
+    from django.contrib.auth import get_user_model
+except ImportError:
+    from django.contrib.auth.models import User
+else:
+    User = get_user_model()
+
+
+# With the default User model these will be 'auth.User' and 'auth.user'
+# so instead of using orm['auth.User'] we can use orm[user_orm_label]
+user_orm_label = '%s.%s' % (User._meta.app_label, User._meta.object_name)
+user_model_label = '%s.%s' % (User._meta.app_label, User._meta.module_name)
+
+
 class Migration(DataMigration):
 
     def forwards(self, orm):
